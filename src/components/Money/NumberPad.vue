26e2999b30b960c89cc227051382f4d2fcda97c7
whYTee <572130960@qq.com>
2022-03-18 22:12:23 +0800

    更新了NumberPad

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,28 +1,64 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button @click="ok" class="ok">OK</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'NumberPad'
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+   output:string = '0'
+
+   inputContent(event:MouseEvent){            //为了避免每次调用时都要传参，直接将参数笼统地设为点击事件
+     const button = (event.target as HTMLButtonElement);
+     const input = button.textContent!;       //！感叹号是ts语法中非null和非undefined的类型断言
+     if (this.output.length >= 16) { return; }
+     if (this.output === '0') {               //output一开始就是0的情况需要做一些特殊判断
+       if ('0123456789'.indexOf(input) >= 0) {
+         this.output = input;
+       } else {
+         this.output += input;
+       }
+       return;
+     }
+     if (this.output.indexOf('.') >= 0 && input === '.') {return;}//防止output出现两个小数点
+     this.output += input;
+   }
+
+   remove(){
+     if (this.output.length === 1) {
+       this.output = '0';
+     } else {
+       this.output = this.output.slice(0, -1);
+     }
+   }
+
+  clear() {
+    this.output = '0';
+  }
+
+  ok(){
+
+  }
 };
 </script>
 
