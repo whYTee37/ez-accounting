f35a8af2a8420cff57ef4ddb32409e5b049284ea
whYTee <572130960@qq.com>
2022-03-19 10:56:54 +0800

    完成Tags中的标签

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,34 +4,33 @@
       <button>新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-<!--      <li>衣</li>-->
-<!--      <li>食</li>-->
-<!--      <li>住</li>-->
-<!--      <li>行</li>-->
-<!--      <li>衣</li>-->
-<!--      <li>食</li>-->
-<!--      <li>住</li>-->
-<!--      <li>行</li>-->
-<!--      <li>衣</li>-->
-<!--      <li>食</li>-->
-<!--      <li>住</li>-->
-<!--      <li>行</li>-->
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected:selectedTags.indexOf(tag)>=0}"
+          @click="select(tag)">
+
+        {{tag}}
+
+      </li>
     </ul>
   </div>
 
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Tags'
+import Vue from 'vue'
+import {Component,Prop} from "vue-property-decorator";
+
+@Component
+export default class Tags extends Vue{
+  @Prop() readonly dataSource:string[]|undefined;
+  selectedTags:string[] = [];
+
+  select(tag:string){
+    const index = this.selectedTags.indexOf(tag);
+    if(index>=0) this.selectedTags.splice(index,1)
+    else this.selectedTags.push(tag);
+  }
+
 };
 </script>
 
@@ -46,7 +45,8 @@ export default {
     display: flex;
     flex-wrap: wrap;
     > li {
-      background: #d9d9d9;
+      $bg: #d9d9d9;
+      background: $bg;
       $h: 24px;
       height: $h;
       line-height: $h;
@@ -54,7 +54,12 @@ export default {
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+      &.selected{
+        background: darken($bg,50%);
+        color: white;
+      }
     }
+
   }
   > .new {
     padding-top: 16px;
